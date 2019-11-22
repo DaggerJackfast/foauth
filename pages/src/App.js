e3171daf3b9bdb43e0154fa8d6c430a2bf82f2e6
Aibek <aibek.abdykasymov@gmail.com>
2019-11-23 01:11:43 +0600

    Added frontend side

---
+++ pages/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import './App.css';
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
+
+
+class App extends Component {
+  render() {
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    return (
+      <div className="App">
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+        <div className="container">
+
+
+          <div className="box">
+            <div className="box-item">
+              Client side flow
+              <div>
+                <FacebookLogin
+                  appId=""
+                  fields="name,email,picture"
+                  autoLoad={false}
+                  redirectUri="https://localhost:3000"
+                  callback={responseFacebook}
+                />
+              </div>
+              <div>
+                <GoogleLogin
+                  clientId=""
+                  buttonText="LOGIN WITH GOOGLE"
+                  uxMode="redirect"
+                  redirectUri="https://localhost:3000"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                />
+              </div>
+            </div>
+            <div className="box-item">
+              Link redirect flow
+              <div>
+                <a href="https://www.facebook.com/dialog/oauth/?client_id=<clientid>&redirect_uri=https://localhost:3000scope=name,email,picture">LOGIN!</a>
+              </div>
+              <div>
+
+              </div>
+            </div>
+            <div className="box-item">
+              Server side flow
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
