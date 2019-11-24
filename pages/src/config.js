a71a324ebc39d97e9c68b5ba2412f374c437ece1
Aibek <aibek.abdykasymov@gmail.com>
2019-11-24 22:55:19 +0600

    Added loading key variables for env

---
+++ pages/src/config.js
@@ -0,0 +1,3 @@
+export const DEV_BACK_URL = process.env.REACT_APP_DEV_BACK_URL;
+export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+export const FACEBOOK_CLIENT_ID = process.env.REACT_APP_FACEBOOK_CLIENT_ID;
